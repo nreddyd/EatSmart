2326e1d39442b8cf9f90f1a7e3ee7b93a9c6b002
Neeraja <neerajareddy24@gmail.com>
2018-07-28 22:31:43 -0400

    updated index.html and main.js to display the images of recipes

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   $("select").formSelect();
 
+  var recipes = [];
+
   $("#submit").on("click", function(e) {
     event.preventDefault();
     var searchPhrase = $("#meal").val();
@@ -29,6 +31,30 @@ $(document).ready(function() {
     })
       .then(res => {
         console.log(res);
+        for (var i = 1; i < 6; i++) {
+          // object to hold the recipe details of the search result
+          var recipe = {
+            id: "",
+            img: "",
+            source: "",
+            course: ""
+          };
+
+          // update the html img tags with the images of recipes from the search result
+          $("#BF" + i).attr("src", res.matches[i].smallImageUrls);
+
+          // assign the id with the search result recipe id
+          recipe.id = res.matches[i].id;
+
+          // assign the img with the search result recipe img
+          recipe.img = res.matches[i].smallImageUrls.toString();
+
+          // assign the img with the search result recipe course
+          recipe.course = res.matches[i].attributes.course[0];
+
+          //push the recipe details to an array
+          recipes.push(recipe);
+        }
       })
       .catch(err => {
         console.log(err);
