a2ceb97ee6371dcc229d61d0a3b62d1236c96cb4
Neeraja <neerajareddy24@gmail.com>
2018-07-30 14:18:36 -0400

    updated files with add to meal plan functionality

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,51 +1,57 @@
 $(document).ready(function() {
   // Materlize functionality
-  $('.sidenav').sidenav();
-  $('select').formSelect();
+  $(".sidenav").sidenav();
+  $("select").formSelect();
 
+  var mealPlan = {
+    breakfast: "",
+    lunch: "",
+    dinner: "",
+    snack: ""
+  };
   // Initialize Firebase
   var config = {
-    apiKey: 'AIzaSyBHDCWBQo0xRH-To2V5bfxNdBKU0FNuffs',
-    authDomain: 'eatsmart-group.firebaseapp.com',
-    databaseURL: 'https://eatsmart-group.firebaseio.com',
-    projectId: 'eatsmart-group',
-    storageBucket: 'eatsmart-group.appspot.com',
-    messagingSenderId: '9553700006'
+    apiKey: "AIzaSyBHDCWBQo0xRH-To2V5bfxNdBKU0FNuffs",
+    authDomain: "eatsmart-group.firebaseapp.com",
+    databaseURL: "https://eatsmart-group.firebaseio.com",
+    projectId: "eatsmart-group",
+    storageBucket: "eatsmart-group.appspot.com",
+    messagingSenderId: "9553700006"
   };
   firebase.initializeApp(config);
 
   // Firebase Google Authorization
   var database = firebase.database();
-  signInButtonElement = $('#sign-in');
-  signOutButtonElement = $('#sign-out');
+  signInButtonElement = $("#sign-in");
+  signOutButtonElement = $("#sign-out");
 
-  signInButtonElement.on('click', signIn);
-  signOutButtonElement.on('click', signOut);
+  signInButtonElement.on("click", signIn);
+  signOutButtonElement.on("click", signOut);
 
-  $('#submit').on('click', function(e) {
+  $("#submit").on("click", function(e) {
     event.preventDefault();
-    var searchPhrase = $('#meal').val();
-    var allergies = $('#allergy').val();
-    var diets = $('#diet').val();
-    var cuisines = $('#cuisine').val();
-    var courses = $('#course').val();
-    var holidays = $('#holiday').val();
-    var time = parseInt($('#time').val()) * 60;
+    var searchPhrase = $("#meal").val();
+    var allergies = $("#allergy").val();
+    // var diets = $('#diet').val();
+    // var cuisines = $('#cuisine').val();
+    // var courses = $('#course').val();
+    // var holidays = $('#holiday').val();
+    // var time = parseInt($('#time').val()) * 60;
 
-    var requiredPictures = true;
+    // var requiredPictures = true;
     var url = `http://api.yummly.com/v1/api/recipes?_app_id=6fe80130&_app_key=e47479bfbd3e29b4ddd5ceb95d60916f&q=${searchPhrase.replace(
-      ' ',
-      '+'
+      " ",
+      "+"
     )}&requirePictures=true${allergies
       .map(allergy => {
-        return '&allowedAllergy[]=' + allergy;
+        return "&allowedAllergy[]=" + allergy;
       })
-      .join('')}`;
+      .join("")}`;
     console.log(url);
 
     getRecipes(url)
       .then(res => {
-        $('#recipeList').empty();
+        $("#recipeList").empty();
         console.log(res);
         for (var i = 1; i < res.matches.length; i++) {
           var image;
@@ -61,30 +67,88 @@ $(document).ready(function() {
   });
 
   // Get recipe link
-  $(document).on('click', '.recipe', function(event) {
+  $(document).on("click", ".recipe", function(event) {
     var recipeid = event.currentTarget.id;
 
     var url =
-      'http://api.yummly.com/v1/api/recipe/' +
+      "http://api.yummly.com/v1/api/recipe/" +
       recipeid +
-      '?_app_id=6fe80130&_app_key=e47479bfbd3e29b4ddd5ceb95d60916f';
+      "?_app_id=6fe80130&_app_key=e47479bfbd3e29b4ddd5ceb95d60916f";
 
-      getRecipes(url).then(res => {
-        console.log(res)
-      }
-      )
+    getRecipes(url).then(res => {
+      console.log(res);
+    });
 
     $(
-      '#recipeButtons'
+      "#recipeContent"
+    ).html(`  <select class="browser-default" id="mealPlanOption">
+    <option value="" disabled selected>Choose your option</option>
+    <option value="breakfast">Breakfast</option>
+    <option value="lunch">Lunch</option>
+    <option value="dinner">Dinner</option>
+    <option value="snack">Snack</option>
+
+  </select>      
+      `);
+    $(
+      "#recipeButtons"
     ).html(`        <button class="btn waves-effect waves-light" type="submit" name="action" id="submit">
         <i class="material-icons right">thumb_up</i>
       </button>
       <button class="btn waves-effect waves-light" type="submit" name="action" id="submit">Get Recipe!
         <i class="material-icons right">send</i>
       </button>
-      <button class="btn waves-effect waves-light" type="submit" name="action" id="submit">Add to meal plan
+      <button class="btn waves-effect waves-light" type="submit" name="action" id="addToMealPlan">Add to meal plan
         <i class="material-icons right">send</i>
       </button>`);
+
+    $("#addToMealPlan").on("click", function() {
+      var course = $("#mealPlanOption").val();
+      switch (course) {
+        case "breakfast":
+          mealPlan.breakfast.id = recipeid;
+          getRecipes(url).then(res => {
+            $("#breakfast").html(`<div> <img src = ${
+              res.images[0].hostedSmallUrl
+            }>
+            <b><p> ${recipeid
+              .split("-")
+              .slice(0, -1)
+              .join(" ")}</p></b></div>`);
+          });
+          break;
+        case "lunch":
+          mealPlan.lunch.id = recipeid;
+          getRecipes(url).then(res => {
+            $("#lunch").html(`<div> <img src = ${res.images[0].hostedSmallUrl}>
+            <b><p> ${recipeid
+              .split("-")
+              .slice(0, -1)
+              .join(" ")}</p></b></div>`);
+          });
+          break;
+        case "dinner":
+          mealPlan.dinner.id = recipeid;
+          getRecipes(url).then(res => {
+            $("#dinner").html(`<div> <img src = ${res.images[0].hostedSmallUrl}>
+            <b><p> ${recipeid
+              .split("-")
+              .slice(0, -1)
+              .join(" ")}</p></b></div>`);
+          });
+          break;
+        case "snack":
+          mealPlan.snack.id = recipeid;
+          getRecipes(url).then(res => {
+            $("#snacks").html(`<div> <img src = ${res.images[0].hostedSmallUrl}>
+            <b><p> ${recipeid
+              .split("-")
+              .slice(0, -1)
+              .join(" ")}</p></b></div>`);
+          });
+          break;
+      }
+    });
   });
 
   // Supplementary Functions
@@ -92,7 +156,7 @@ $(document).ready(function() {
   async function getRecipes(url) {
     const result = await $.ajax({
       url: url,
-      method: 'GET'
+      method: "GET"
     });
 
     return result;
@@ -104,19 +168,31 @@ $(document).ready(function() {
     // image.attr('src', recipe.smallImageUrls);
     // image.attr('id', recipe.id);
 
-    var recipeDiv = $('<div>');
-    recipeDiv.addClass('card horizontal');
+    var recipeDiv = $("<div>");
+    recipeDiv.addClass("card horizontal");
     recipeDiv.html(`
       <div class="card-stacked">
         <div class="card-content">
             <img id=${recipe.id} src=${recipe.smallImageUrls}>
+            <br>
             <a id=${recipe.id} class="recipe">${recipe.id
-      .split('-')
+      .split("-")
       .slice(0, -1)
-      .join(' ')}</a>
+      .join(" ")}</a>
         </div>
       </div>`);
 
-    $('#recipeList').append(recipeDiv);
+    $("#recipeList").append(recipeDiv);
   }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll("select");
+  var instances = M.FormSelect.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function() {
+  $("select").formSelect();
 });
