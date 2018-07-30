8858a427754662a5c12ef0b6fd9f826221a02304
RubyG <rubyrgill@hotmail.com>
2018-07-29 20:35:39 -0400

    seperated files to align with website layout

---
+++ assets/javascript/loginapp.js
@@ -0,0 +1,76 @@
+var database = firebase.database();
+
+//LOGGING IN
+//---------------------------------------
+$("#log-in").on("click", e => {
+
+    event.preventDefault();
+    var userEmail = $("#user-email").val().trim();
+    var userPass = $("#user-password").val().trim();
+    var userDiet = $("#input-diet").val();
+    var auth = firebase.auth();
+
+    //checking to see value input
+
+    console.log("User Name is: " + userEmail + " Password is: " + userPass)
+
+    if (userEmail == "") {
+        alert("missing email")
+    }
+    if (userPass == "") {
+        alert("missing Pass")
+    } else {
+        alert("Welcome : " + userEmail)
+
+    }
+
+
+    auth.signInWithEmailAndPassword(userEmail, userPass)
+        .then(function (user) {
+            console.log(database.ref('/users/' + user.uid));
+        }).catch(function (error) {
+            console.log(error.code);
+            console.log(error.message)
+        });
+    alert("YOUR ARE LOGGED IN")
+
+});
+
+
+
+//Check if user is signed in
+function isUserSignedIn() {
+    return !!firebase.auth().currentUser;
+    console.log("isUserSignedIn" + isUserSignedIn)
+}
+
+
+//CHECK STATUS
+//---------------------------------------
+firebase.auth().onAuthStateChanged(function (user) {
+
+
+    console.log("user:", user);
+
+
+    if (user != null) {
+        event.preventDefault();
+        $("#sign-out").removeClass("hide");
+        $("#intro").append("WELCOME: " + user.email)
+    }
+
+    else {
+
+        console.log("not logged in");
+        $("#sign-out").addClass("hide");
+        $("#profile").addClass("hide");
+    }
+});
+
+
+
+
+
+
+
+
--- assets/javascript/registerapp.js
+++ assets/javascript/registerapp.js
@@ -1,42 +1,24 @@
 var database = firebase.database();
 
-var userUID;
-
-
-//If user selected input checkbox
-// function ischeckBox() {
-
-//     var peanuts = $("#input-allergy-peanuts").val();
-
-//     alert(peanuts);
-//     if (peanuts.checked == true) {
-//         console.log("no," + peanuts);
-//     } else {
-//         console.log("not allergic");
-//     }
-// }
-// ischeckBox()
-
 //NEW USER
 //---------------------------------------
-$("#submit-register").on("click", e => {
 
+var userUID;
 
+$("#submit-register").on("click", function (event) {
 
     event.preventDefault();
-
-
 
     var userName = $("#user-name").val().trim();
     var userEmail = $("#register-email").val().trim();
     var userPass = $("#register-password").val().trim();
     var auth = firebase.auth();
 
+
     var dietType = $("#input-diet").val().trim();
 
-
-
-    console.log(userName + "  " + userEmail + "  " + userPass + "  " + dietType)
+    console.log(
+        "email:" + userEmail + "Pass:" + userPass + "name:" + userName + "diet:" + dietType);
 
 
     if (userEmail == "") {
@@ -47,120 +29,31 @@ $("#submit-register").on("click", e => {
     } else {
         alert("Welcome :  " + userName)
         alert("allgood")
-        $("#register-form").removeClass("hide");
-        $("#login-form").addClass("hide");
-        $("#user-name").addClass("hide");
-        $("#register-email").addClass("hide");
-        $("#register-password").addClass("hide");
-        $("#submit-register").addClass("hide");
-        $("#input-diet").addClass("hide");
-        $("#allergy-options").addClass("hide")
-        $("#newuser-sign-out").removeClass("hide");
-
-
-
     }
 
-    var promise = auth.createUserWithEmailAndPassword(userEmail, userPass).then(function (user) {
-        return user.updateProfile({ displayName: $("#user-name").val() });
+    auth.createUserWithEmailAndPassword(userEmail, userPass).then(function (user) {
 
         userUID = user.uid;
 
-        database.ref('/Users/' + userUID).set({
-            'name': userName,
-            'email': userEmail,
-            'password': userPassword,
-            'diet': dietType
-        })
-
-        console.log("the diet is" + users.diet.dietType)
-    });
-    promise.catch(e => console.log(e.message));
-});
-
-
-
-
-
-
-
-
-
-
-
-
-//PUSHING INPUT TO DATABASE
-//---------------------------------------
-//Pushing new user data onto database
-$("#submit-register").on("click", function () {
-
-    database.ref().on("child_added", function (childSnap) {
-        //grabbing input value
-        theName = childSnap.val().theName;
-        theEmail = childSnap.val().theEmail;
-        thePass = childSnap.val().thePass;
-        theDiet = childSnap.val().theDiet
+        // create a new Node
+        database.ref("/Users/" + userUID).set({
+            "email": userEmail,
+            "name": userName,
+            "pass": userPass,
+            "diet": dietType
+        });
     })
-
-    var theName = $("#user-name").val().trim();
-    var theEmail = $("#register-email").val().trim();
-    var thePass = $("#register-password").val().trim();
-    var theDiet = $("#input-diet").val().trim();
-
-
-
-    //object
-    var newUser = {
-        'theName': theName,
-        'theEmail': theEmail,
-        'thePass': thePass,
-        'theDiet': theDiet
-    }
-
-    database.ref().push(newUser);
-    console.log(newUser)
-
-
-    $("#the-diet").append(newUser.theDiet);
-
-    $("#diet-options").addClass("hide");
-    $("#newuser-sign-out").addClass("hide");
-    $("#sign-in").addClass("hide");
-
+        .catch(function (error) {
+            console.log(error.code);
+            console.log(error.message);
+        })
+        .then(function () {
+            window.location.href = "Homepage.html";
+            $("#intro").append("WELCOME: " + user.email)
+        });
 
 });
 
-//LOGGING IN
-//---------------------------------------
-$("#log-in").on("click", e => {
-
-    event.preventDefault();
-    var userEmail = $("#user-email").val().trim();
-    var userPass = $("#user-password").val().trim();
-    var userDiet = $("#input-diet").val().trim();
-    var auth = firebase.auth();
-
-    //checking to see value input
-
-    console.log("User Name is: " + userEmail + " Password is: " + userPass)
-
-    if (userEmail == "") {
-        alert("missing email")
-    }
-    if (userPass == "") {
-        alert("missing Pass")
-    } else {
-        alert("Welcome :  " + userEmail)
-
-    }
-
-
-
-    var promise = auth.signInWithEmailAndPassword(userEmail, userPass);
-
-    //checking for errors
-    promise.catch(e => console.log(e.message));
-})
 
 
 
@@ -170,11 +63,7 @@ $("#register").on("click", e => {
 
 
     alert("Let's register")
-    $("#register").addClass("hide");
-    $("#register-form").removeClass("hide");
-    $("#login-form").addClass("hide");
-    $("#newuser-sign-out").addClass("hide");
-    $("#sign-in").addClass("hide");
+    window.location.href = "registerindex.html";
 });
 
 //REGISTER BUTTON
@@ -183,76 +72,18 @@ $("#sign-in").on("click", e => {
 
 
     alert("Let's Log In")
-    $("#login-form").removeClass("hide");
-    $("#sign-in").addClass("hide");
-    $("#register").addClass("hide");
-    $("#register-form").addClass("hide");
+    window.location.href = "signin.html";
 });
 
 
 
-//Creating profile references 
-function writeUserData(userId, name, email, imageUrl) {
-    firebase.database().ref('users/' + userId).set({
-        username: name,
-        email: email,
-        profile_picture: imageUrl
-    });
-}
 
 
 //LOGGING OUT
 //---------------------------------------
 $("#sign-out").on("click", e => {
     firebase.auth().signOut();
+
+    $("#intro").addClass("hide");
 });
-
-$("#newuser-sign-out").on("click", e => {
-    firebase.auth().signOut();
-});
-
-
-
-//Check if user is signed in
-function isUserSignedIn() {
-    return !!firebase.auth().currentUser;
-    console.log("isUserSignedIn" + isUserSignedIn)
-}
-
-
-//CHECK STATUS
-//---------------------------------------
-firebase.auth().onAuthStateChanged(function (user) {
-
-    event.preventDefault();
-
-    if (user) {
-
-        event.preventDefault();
-
-        console.log("user:", user);
-        var loggedUser = user.email;
-
-
-        //FIX: firebaseUser not displaying
-
-        $("#sign-out").removeClass("hide");
-        $("#register").addClass("hide");
-        $("#log-in").addClass("hide");
-        $("#user-email").addClass("hide");
-        $("#user-password").addClass("hide");
-        $("#sign-in").addClass("hide");
-
-        $("#intro").append("WELCOME: " + loggedUser)
-        console.log("welcome, " + user.displayName);
-
-        if (user != null) {
-            console.log("")
-        }
-
-    } else {
-        console.log("not logged in  ");
-    }
-});
-
 
