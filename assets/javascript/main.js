61c25b33929569de101f1243e67a67ca26a0a1d8
ale <ale-juarez@hotmail.ca>
2018-07-30 22:12:55 -0400

    display the recipe selected

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,51 +1,51 @@
 $(document).ready(function() {
   // Materlize functionality
-  $('.sidenav').sidenav();
-  $('select').formSelect();
+  $(".sidenav").sidenav();
+  $("select").formSelect();
 
   // Initialize Firebase
   var config = {
-    apiKey: 'AIzaSyBHDCWBQo0xRH-To2V5bfxNdBKU0FNuffs',
-    authDomain: 'eatsmart-group.firebaseapp.com',
-    databaseURL: 'https://eatsmart-group.firebaseio.com',
-    projectId: 'eatsmart-group',
-    storageBucket: 'eatsmart-group.appspot.com',
-    messagingSenderId: '9553700006'
+    apiKey: "AIzaSyBHDCWBQo0xRH-To2V5bfxNdBKU0FNuffs",
+    authDomain: "eatsmart-group.firebaseapp.com",
+    databaseURL: "https://eatsmart-group.firebaseio.com",
+    projectId: "eatsmart-group",
+    storageBucket: "eatsmart-group.appspot.com",
+    messagingSenderId: "9553700006"
   };
   firebase.initializeApp(config);
 
   // Firebase Google Authorization
   var database = firebase.database();
-  signInButtonElement = $('#sign-in');
-  signOutButtonElement = $('#sign-out');
+  signInButtonElement = $("#sign-in");
+  signOutButtonElement = $("#sign-out");
 
-  signInButtonElement.on('click', signIn);
-  signOutButtonElement.on('click', signOut);
+  signInButtonElement.on("click", signIn);
+  signOutButtonElement.on("click", signOut);
 
-  $('#submit').on('click', function(e) {
+  $("#submit").on("click", function(e) {
     event.preventDefault();
-    var searchPhrase = $('#meal').val();
-    var allergies = $('#allergy').val();
-    var diets = $('#diet').val();
-    var cuisines = $('#cuisine').val();
-    var courses = $('#course').val();
-    var holidays = $('#holiday').val();
-    var time = parseInt($('#time').val()) * 60;
+    var searchPhrase = $("#meal").val();
+    var allergies = $("#allergy").val();
+    var diets = $("#diet").val();
+    var cuisines = $("#cuisine").val();
+    var courses = $("#course").val();
+    var holidays = $("#holiday").val();
+    var time = parseInt($("#time").val()) * 60;
 
     var requiredPictures = true;
     var url = `http://api.yummly.com/v1/api/recipes?_app_id=6fe80130&_app_key=e47479bfbd3e29b4ddd5ceb95d60916f&q=${searchPhrase.replace(
-      ' ',
-      '+'
+      " ",
+      "+"
     )}&requirePictures=true${allergies
       .map(allergy => {
-        return '&allowedAllergy[]=' + allergy;
+        return "&allowedAllergy[]=" + allergy;
       })
-      .join('')}`;
+      .join("")}`;
     console.log(url);
 
     getRecipes(url)
       .then(res => {
-        $('#recipeList').empty();
+        $("#recipeList").empty();
         console.log(res);
         for (var i = 1; i < res.matches.length; i++) {
           var image;
@@ -61,21 +61,47 @@ $(document).ready(function() {
   });
 
   // Get recipe link
-  $(document).on('click', '.recipe', function(event) {
+  $(document).on("click", ".recipe", function(event) {
     var recipeid = event.currentTarget.id;
 
     var url =
-      'http://api.yummly.com/v1/api/recipe/' +
+      "http://api.yummly.com/v1/api/recipe/" +
       recipeid +
-      '?_app_id=6fe80130&_app_key=e47479bfbd3e29b4ddd5ceb95d60916f';
+      "?_app_id=6fe80130&_app_key=e47479bfbd3e29b4ddd5ceb95d60916f";
 
-      getRecipes(url).then(res => {
-        console.log(res)
+    getRecipes(url).then(res => {
+      console.log(res);
+      $("#recipeContent").empty();
+      var recipeDiv = $("<div>");
+      // Creating an image tag
+      var recipeImage = $("<img>");
+      // Giving the image tag an src attribute of a proprty pulled off the
+      // result item
+      recipeImage.attr(
+        "src",
+        res.images[0].hostedMediumUrl,
+        "class='center-align'"
+      );
+      // Appending the paragraph (the name, the time, ingredients)
+      var pCourse = res.attributes.course[0];
+      var pTime = res.totalTime;
+      var pName = res.name;
+      var pServings = res.numberOfServings;
+      recipeDiv.append(recipeImage);
+      recipeDiv.append("<h5>" + pName + "</h5>");
+      recipeDiv.append("Course: " + pCourse + "<br>");
+      recipeDiv.append("Time: " + pTime + "<br>");
+      var pIngr = $("<p>").text("Ingredients: ");
+      recipeDiv.append(pIngr);
+      for (var i = 0; i < res.ingredientLines.length; i++) {
+        pIngr = res.ingredientLines[i];
+        recipeDiv.append(pIngr + "<br>");
       }
-      )
+      $("#recipeContent").append(recipeDiv);
+    });
 
     $(
-      '#recipeButtons'
+      "#recipeButtons"
     ).html(`        <button class="btn waves-effect waves-light" type="submit" name="action" id="submit">
         <i class="material-icons right">thumb_up</i>
       </button>
@@ -92,7 +118,7 @@ $(document).ready(function() {
   async function getRecipes(url) {
     const result = await $.ajax({
       url: url,
-      method: 'GET'
+      method: "GET"
     });
 
     return result;
@@ -104,19 +130,19 @@ $(document).ready(function() {
     // image.attr('src', recipe.smallImageUrls);
     // image.attr('id', recipe.id);
 
-    var recipeDiv = $('<div>');
-    recipeDiv.addClass('card horizontal');
+    var recipeDiv = $("<div>");
+    recipeDiv.addClass("card horizontal");
     recipeDiv.html(`
       <div class="card-stacked">
         <div class="card-content">
             <img id=${recipe.id} src=${recipe.smallImageUrls}>
             <a id=${recipe.id} class="recipe">${recipe.id
-      .split('-')
+      .split("-")
       .slice(0, -1)
-      .join(' ')}</a>
+      .join(" ")}</a>
         </div>
       </div>`);
 
-    $('#recipeList').append(recipeDiv);
+    $("#recipeList").append(recipeDiv);
   }
 });
